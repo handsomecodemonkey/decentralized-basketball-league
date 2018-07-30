b5612b22e2ce08e77ff6a5f33eba91b9c4865b31
Erick Kusnadi <erick@handsomecodemonkey.com>
2018-07-29 17:52:17 -0700

    add decentralized team proposal staking

--- contracts/BasketballLeague.sol
+++ contracts/BasketballLeague.sol
@@ -6,13 +6,15 @@ contract BasketballLeague {
 	/** Enums + Structs */
    
     struct Team {
-        string metaDataLink;
+        string name;
+        string metaData;
         address owner; //Will be the address of a team DAO or can be an individual owner
+        uint256 stakedEth;
     }
 
     /** Events */
     
-    event TeamAdded(uint256 teamId);
+    event TeamAdded(uint256 teamId, string teamName);
     //event PlayerDrafted(uint256 assetId, uint256 teamId);
     //event PlayerRenounced(uint256 assetId, uint256 teamId);
     //event CommisionerChanged(address oldCommisioner, address newCommisioner);
@@ -31,6 +33,10 @@ contract BasketballLeague {
     uint256 private teamCount;
     mapping(uint256 => Team) private teams;
 
+    //Proposals
+    uint256 private proposalCount;
+    mapping(uint256 => Team) public proposedTeams;
+
     /*
     uint256 private teamCount;
     mapping(uint256 => Team) private teams;
@@ -42,6 +48,11 @@ contract BasketballLeague {
         _;
     }
 
+    modifier requiresStakedEth(uint _amount) {
+    	require( (msg.value != 0) && (msg.value / (1 ether) >= _amount) );
+    	_;
+    }
+
 	constructor(uint256 _maxTeamSalary, uint256 _maxTeamSize) public {
 	    commisioner = msg.sender;
 	    maxTeamSalary = _maxTeamSalary;
@@ -51,6 +62,12 @@ contract BasketballLeague {
 	    //emergencyStop = false;
 	}
 
+	//Returns total eth in the contract
+	function ethPool() public view returns(uint) {
+		return address(this).balance;
+	}
+
+	//Commisioner only functions
     function setMaxTeamSalary(uint256 _maxTeamSalary) public onlyCommisioner {
         maxTeamSalary = _maxTeamSalary;
     }
@@ -59,11 +76,32 @@ contract BasketballLeague {
         maxTeamSize = _maxTeamSize;
     }
 
-    function createNewTeam(string _metaData, address _owner) public onlyCommisioner {
+    function approveTeamProposal(uint256 _proposalId) public onlyCommisioner {
+    	//TODO make sure you don't approve twice
+    	Team memory team = proposedTeams[_proposalId];
+    	delete proposedTeams[_proposalId];
+    	createNewTeam(team);
+    }
+
+    function withdrawTeamProposal(uint256 _proposalId) public {
+    	//TODO make sure you can't do twice
+    	Team memory team = proposedTeams[_proposalId];
+    	require(msg.sender == team.owner);
+    	delete proposedTeams[_proposalId];
+    	msg.sender.transfer(team.stakedEth);
+    }
+
+    //public functions
+    function proposeNewTeam(string _name, string _metaData) public payable requiresStakedEth(maxTeamSalary) {
+    	proposalCount++;
+    	Team memory newTeam = Team(_name, _metaData, msg.sender, msg.value);
+    	proposedTeams[proposalCount] = newTeam;
+    }
+
+    function createNewTeam(Team _newTeam) internal {
         teamCount++;
-        Team memory newTeam = Team(_metaData, _owner);
-        teams[teamCount] = newTeam;
-        emit TeamAdded(teamCount);
+        teams[teamCount] = _newTeam;
+        emit TeamAdded(teamCount, _newTeam.name);
     }
 
 }
